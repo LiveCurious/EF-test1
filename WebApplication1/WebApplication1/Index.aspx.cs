4641a4e0b91352c31563d421bf99ae885cdc1d06
LiveCurious <chervonaolga@gmail.com>
2015-08-24 21:45:32 +0800

    fixed initialization of the values

--- WebApplication1/WebApplication1/Index.aspx.cs
+++ WebApplication1/WebApplication1/Index.aspx.cs
@@ -13,11 +13,59 @@ namespace test
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+
+            InitializeValues();
             using (var ctx = new Context())
             {
-               Employee stud = new Employee() { EmployeeId  =  Guid.NewGuid(), Name = "Test Student", DateOfBith = new DateTime(2000, 12, 12) };
-                
-               ctx.Employees.Add(stud);
+                var list =  from s in ctx.Employees
+                            select s.Name;
+
+                var t = 10;
+            }
+        }
+
+        private void InitializeValues()
+        {
+            using (var ctx = new Context())
+            {
+
+                IList<Employee> listOfEmployees = new List<Employee>();
+
+                listOfEmployees.Add(new Employee()
+                {
+                    EmployeeId = Guid.NewGuid(),
+                    Name = "Vasya Pupkin",
+                    DateOfBith = new DateTime(1991, 12, 12)
+                });
+                listOfEmployees.Add(new Employee()
+                {
+                    EmployeeId = Guid.NewGuid(),
+                    Name = "Petya Vasechkin",
+                    DateOfBith = new DateTime(1992, 03, 23)
+                });
+                listOfEmployees.Add(new Employee()
+                {
+                    EmployeeId = Guid.NewGuid(),
+                    Name = "Kolya Petrov",
+                    DateOfBith = new DateTime(1993, 04, 04)
+                });
+
+                foreach (Employee std in listOfEmployees)
+                {
+                    ctx.Employees.Add(std);
+                }
+                IList<Training> listOfTrainings = new List<Training>();
+
+                listOfTrainings.Add(new Training() {TrainingId = Guid.NewGuid(), Name = "Math", Description = "101"});
+                listOfTrainings.Add(new Training() {TrainingId = Guid.NewGuid(), Name = "Physics", Description = "101"});
+                listOfTrainings.Add(new Training() {TrainingId = Guid.NewGuid(), Name = "Stats", Description = "102"});
+
+                foreach (Training std in listOfTrainings)
+                {
+                    ctx.Trainings.Add(std);
+
+                }
+
                 ctx.SaveChanges();
             }
         }
